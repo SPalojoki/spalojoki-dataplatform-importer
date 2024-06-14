f2e8d3923b43dbe0809dc6e65fc7bba63ac530fe
Sakari Palojoki <sakari.palojoki@kone.com>
2024-06-14 15:56:11 +0300

    Fix test function

--- test_function/main.py
+++ test_function/main.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+import functions_framework
+
+
+@functions_framework.cloud_event
+def hello_world(event):
+    """Cloud Event Function.
+    Args:
+        event: Cloud event for the function trigger
+    """
+    print("Hello, stdout!")
